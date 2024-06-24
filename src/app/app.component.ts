2745eaba71b8ad0ca0890db0168313108bfe7c47
Pedro Brion <pedro.brion@gmail.com>
2024-06-24 15:30:21 -0300

    wip: authing user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { isPlatformBrowser } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, PLATFORM_ID, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SsrCookieService } from 'ngx-cookie-service-ssr';
@@ -16,6 +16,7 @@ export class AppComponent {
   platformId = inject(PLATFORM_ID);
   id: string = '';
   name: string = '';
+  access_token: string = '';
   constructor(private http: HttpClient, private cookie: SsrCookieService) {
     this.initialize();
   }
@@ -26,25 +27,25 @@ export class AppComponent {
 
   async getUserId() {
     this.id = this.cookie.get('userId');
+    this.access_token = this.cookie.get('access_token');
 
     // if (isPlatformBrowser(this.platformId)) {
     if (isPlatformBrowser(this.platformId)) {
       try {
-        this.id
-          ? this.http
-              .get(`http://localhost:3000/users/${this.id}`)
-              .subscribe((res: any) => {
-                this.id = res.id;
-                this.name = res.name;
-                this.cookie.set('userId', this.id, 12000, '/');
-              })
-          : this.http
-              .get('http://localhost:3000/users/new')
-              .subscribe((res: any) => {
-                this.id = res.id;
-                this.name = res.name;
-                this.cookie.set('userId', this.id, 12000, '/');
-              });
+        this.http
+          .get(`http://localhost:3000/auth/me`, {
+            headers: new HttpHeaders({
+              Authorization: `Bearer ${this.access_token}`,
+            }),
+          })
+          .subscribe((res: any) => {
+            console.log(res);
+            this.id = res.user.id;
+            this.name = res.user.name;
+            this.access_token = res.access_token;
+            this.cookie.set('userId', this.id, 12000, '/');
+            this.cookie.set('access_token', this.access_token, 12000, '/');
+          });
       } catch (err) {
         console.error(err);
       }
